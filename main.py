57ce5f4c409eae60c601688ba9987084dab76282
Tod Lazarov <tod.lazarov@gmail.com>
2025-04-14 22:54:21 -0400

    Reformat the count function into its own file

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+from stats import count_words
+
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
 
         return file_contents
-
-def count_words(text):
-    count = len(text.split())
-
-    return count
 
 def main():
     book = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(text):
+    count = len(text.split())
+
+    return count
