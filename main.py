52e1b00203497476bc97734327339aee808cf4d9
Tod Lazarov <tod.lazarov@gmail.com>
2025-04-15 01:09:23 -0400

    Add a function that counts occurence of each character in the book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_characters
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,8 +9,10 @@ def get_book_text(path):
 
 def main():
     book = get_book_text("books/frankenstein.txt")
+    characters = count_characters(book)
     final_count = count_words(book)
-
+    
     print("{0} words found in the document".format(final_count))
+    print(characters)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def count_words(text):
     count = len(text.split())
 
     return count
+
+def count_characters(text):
+    character_dict = {}
+
+    for i in text:
+        lowerchar = i.lower()
+        if lowerchar in character_dict:
+            character_dict[lowerchar] += 1
+        else:
+            character_dict[lowerchar] = 1
+
+    return character_dict
